37a8becf5dd5088dc4072d7b0bd984c11929de66
Silver Stonks <85765392+Silverstonks@users.noreply.github.com>
2022-06-30 13:42:30 -0400

    Update Sections.jsx

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -17,25 +17,29 @@ import FoundryIcon from '../static/images/slider/IconFoundry.png';
 import DotsDecoration from '../static/images/slider/DotsDecoration.png';
 import StakingIcon from '../static/images/slider/IconStaking.png';
 import StakingImg from '../static/images/sections/SectionStaking.gif';
+
 import { useTranslation } from 'react-i18next';
 
 const Sections = () => {
   return (
     <div className="container mx-auto sections">
-      <SectionItemImgRight id="propertyholders" icon={PropertyHoldersIcon} img={PropertyHoldersImg} btnHref={`https://propertyholders.io/`} />
+      <SectionItemImgRight id="bidsarena" icon={BidsarenaIcon} img={BidsarenaImg} btnHref={'../static/images/SilverBankRun.pdf'} />
       <div className="d-flex justify-content-end ">
         <img src={DotsDecoration} alt="" className="mr-5 my-4" />
       </div>
-      <SectionItemImgLeft id="silvershop" icon={SilverShopIcon} img={SilverShopImg} btnHref={`https://silverstonks.com/`} />
+      <SectionItemImgLeft id="foundry" icon={FoundryIcon} img={FoundryImg} btnHref={`../static/images/SilverBankRun.pdf`}/>
       <SectionItemImgRight id="vault" icon={VaultIcon} img={VaultImg} btnHref={`https://collectibles.silverstonks.com/`} />
+      <SectionItemImgLeft id="silvershop" icon={SilverShopIcon} img={SilverShopImg} btnHref={`https://silverstonks.com/`} />
+      <SectionItemImgRight id="propertyholders" icon={PropertyHoldersIcon} img={PropertyHoldersImg} btnHref={`https://propertyholders.io/`} />
       <SectionItemImgLeft id="lounge" icon={LoungeIcon} img={LoungeImg} btnHref={`https://discord.com/invite/6q9hQcmYq5`} />
       <SectionItemImgRight id="exchange" icon={ExchangeIcon} img={ExchangeImg} btnHref={`https://silverstonks.io`} />
       <div className="d-flex justify-content-center ">
         <img src={DotsDecoration} alt="" className="mr-5 my-4" style={{ transform: 'rotate(90deg)' }} />
       </div>
-      {/*<SectionItemImgLeft id="foundry" icon={FoundryIcon} img={FoundryImg} />*/}
       <SectionItemImgLeft id="staking" icon={StakingIcon} img={StakingImg}  btnHref={`https://staking.silverstonkstoken.com/`}/>
-      <SectionItemImgRight id="bidsarena" icon={BidsarenaIcon} img={BidsarenaImg} btnHref={'https://bidsarena.com/'} />
+      {/*<SectionItemImgLeft id="foundry" icon={FoundryIcon} img={FoundryImg} />*/}
+      {/*<SectionItemImgLeft id="staking" icon={StakingIcon} img={StakingImg}  btnHref={`https://staking.silverstonkstoken.com/`}/>*/}
+      
     </div>
   );
 };
